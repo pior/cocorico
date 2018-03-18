23c66cb04c06fc225bf2632c5c66a37be1fa8911
Pior Bastida <pior@pbastida.net>
2018-03-18 17:19:58 -0400

    autopush

--- cocorico/display/__init__.py
+++ cocorico/display/__init__.py
@@ -2,6 +2,8 @@ from luma.core.interface.serial import spi as luma_spi
 from luma.core.render import canvas
 from luma.oled.device import ssd1306
 
+from PIL import ImageFont
+
 from cocorico.hal.spi import SpiDev
 from cocorico.hal.gpio import GPIO
 
@@ -12,7 +14,9 @@ class Display:
         serial = luma_spi(spi=SpiDev(), gpio=GPIO, device=0, port=0)
         self._device = ssd1306(serial)
 
+        self._large_font = ImageFont.truetype('fonts/digital-7-mono.ttf', 36)
+
     def announce(self, text):
         with canvas(self._device) as draw:
             draw.rectangle(self._device.bounding_box, outline="white", fill="black")
-            draw.text((30, 25), text, fill="white")
+            draw.text((30, 25), text, font=self._large_font, fill="white")
