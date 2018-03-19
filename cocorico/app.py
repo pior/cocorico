fcff318e2ee3fbcff25ebc5cb527a1fa44ce7cec
Pior Bastida <pior@pbastida.net>
2018-03-19 00:43:00 -0400

    autopush

--- cocorico/app.py
+++ cocorico/app.py
@@ -47,13 +47,13 @@ class App():
         if state == State.CLOCK:
             if self.alarm_settings.active:
                 alarm_time = self.alarm_settings.time.strftime('%H:%M')
-                text = 'Alarm: %s' % alarm_time
+                text = '     %s' % alarm_time
             else:
                 text = ''
             self.display.as_clock(self.clock.time, text)
 
         elif state == State.ALARM:
-            self.display.as_clock(self.clock.time, 'ALARM!!!')
+            self.display.as_clock(self.clock.time, ' /!\ ALARM /!\\')
 
         elif state == State.ALARM_TIME:
             self.display.as_clock(self.alarm_settings.time, 'SET TIME')
