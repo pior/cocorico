99924cbc8b8821f84a564c3bd53649a2dd54368a
Pior Bastida <pior@pbastida.net>
2018-03-23 00:38:54 -0400

    autopush

--- cocorico/sound/amplifier.py
+++ cocorico/sound/amplifier.py
@@ -5,15 +5,15 @@ class Amplifier:
     SHUTDOWN_PIN = 4  # BCM pin 4
 
     def __init__(self):
-        GPIO.setup(self.SHUTDOWN_PIN, GPIO.OUT, initial=GPIO.LOW)
-        self._shutdown = False
+        GPIO.setup(self.SHUTDOWN_PIN, GPIO.OUT, initial=GPIO.HIGH)
+        self._active = False
 
     def enable(self):
-        if self._shutdown:
-            GPIO.output(self.SHUTDOWN_PIN, GPIO.HIGH)
-            self._shutdown = False
+        if not self._active:
+            GPIO.output(self.SHUTDOWN_PIN, GPIO.LOW)
+            self._active = True
 
     def disable(self):
-        if not self._shutdown:
-            GPIO.output(self.SHUTDOWN_PIN, GPIO.LOW)
-            self._shutdown = True
+        if self._active:
+            GPIO.output(self.SHUTDOWN_PIN, GPIO.HIGH)
+            self._active = False
--- cocorico/sound/engine/alsa.py
+++ cocorico/sound/engine/alsa.py
@@ -1,4 +1,8 @@
+import logging
+
 import simpleaudio
+
+log = logging.getLogger(__name__)
 
 
 class AlsaPlayer:
@@ -7,10 +11,14 @@ class AlsaPlayer:
 
     def start(self, path):
         wave_object = simpleaudio.WaveObject.from_wave_file(path)
+        log.info("Wave object from %s: %s", path, wave_object)
+
         self._play_obj = wave_object.play()
+        log.info("Play object: %s", self._play_obj)
 
     def stop(self):
         if self._play_obj:
+            log.info("Stop: %s", self._play_obj)
             self._play_obj.stop()
         self._play_obj = None
 
