5c5b31a894949fccf0b25e402f19052f1c0ad1ca
Pior Bastida <pior@pbastida.net>
2018-03-25 22:33:20 -0400

    Simplify and log amplifier

--- cocorico/sound/amplifier.py
+++ cocorico/sound/amplifier.py
@@ -1,22 +1,26 @@
+import logging
+
 from cocorico.hal.gpio import GPIO
+
+log = logging.getLogger(__name__)
 
 
 class Amplifier:
     SHUTDOWN_PIN = 4  # BCM pin 4
+    ENABLED = GPIO.LOW
+    DISABLE = GPIO.HIGH
 
     def __init__(self):
-        GPIO.setup(self.SHUTDOWN_PIN, GPIO.OUT, initial=GPIO.HIGH)
-        self._active = False
+        GPIO.setup(self.SHUTDOWN_PIN, GPIO.OUT, initial=self.DISABLE)
+
 
     def enable(self):
-        if not self._active:
-            GPIO.output(self.SHUTDOWN_PIN, GPIO.LOW)
-            self._active = True
+        log.info("Enable amplifier")
+        GPIO.output(self.SHUTDOWN_PIN, self.ENABLE)
 
     def disable(self):
-        if self._active:
-            GPIO.output(self.SHUTDOWN_PIN, GPIO.HIGH)
-            self._active = False
+        log.info("Disable amplifier")
+        GPIO.output(self.SHUTDOWN_PIN, self.DISABLE)
 
     def close(self):
         GPIO.cleanup(self.SHUTDOWN_PIN)
